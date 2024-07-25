6fc103d395a5d7f06cd9f1a0e4d153b1263532b8
JJAJJAKIM <csm9121@gmail.com>
2024-07-25 16:14:47 +0900

    DB 연결 추가

--- src/main/java/com/app/controller/WebController.java
+++ src/main/java/com/app/controller/WebController.java
@@ -1,13 +1,17 @@
 package com.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
 
+    @Autowired
+    private TestMapper tm;
+
     @GetMapping("/")
     public String home() {
-        return "하잉 하잉";
+        return "하잉하잉 (" + tm.testDB() +")";
     }
 }
